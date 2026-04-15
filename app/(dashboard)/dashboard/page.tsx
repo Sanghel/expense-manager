0eaeb5369f521316d062c72532d7ea8bc8fbf0e1
Sanghel González <sanghelgonzalez@gmail.com>
2026-04-14 21:13:34 -0500

    feat: create dashboard layout and route structure

---
+++ app/(dashboard)/dashboard/page.tsx
@@ -0,0 +1,11 @@
+'use client'
+
+import { Box, Heading } from '@chakra-ui/react'
+
+export default function DashboardPage() {
+  return (
+    <Box>
+      <Heading mb={8}>Dashboard</Heading>
+    </Box>
+  )
+}
